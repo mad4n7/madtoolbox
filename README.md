6fc5de438dd3c8e28f1c0023ca32f0140e4f2282
Arthur S <me@mad4n7.com>
2022-05-19 10:12:05 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# toolbox
+A bunch of separated functionalities for multiple purposes
+
+## Requirements:
+. Python 3.8 and up
+
+
+# Files Structure
+* /files/
+  * `remove_special_characters.py`: Removes special characters from files in a folder but keeps `-` and `_`
+  
+
---
+++ files/remove_special_characters.py
@@ -0,0 +1,40 @@
+############################
+# Version: 1.0.1
+# @author me@mad4n7.com
+############################
+# TODO: validate multiples .(dots) in a string
+# TODO: if there is no /(slash) at the end of the line add
+# TODO: rename files in the folder (recursive)
+
+import os
+from os import listdir
+from os import path
+
+VALID_CHARACTERS = ['-', '_']
+print("######################################################################")
+print('Example: ' + os.getcwd() + '/')
+print("######################################################################")
+
+folder_path = input('Folder path: ')
+
+if folder_path == 'pwd':
+    print('Suggested folder: ' + os.getcwd() + '/')
+    folder_path = input('Sugested folder: ', )
+
+def getModifiedPath(originalPath):
+    result = ''
+    file_name = originalPath.split('.')
+    for c in file_name[0]:
+        if c in VALID_CHARACTERS or c.isalpha() or c.isnumeric():
+            result += c
+
+    result += '.' + file_name[1]
+    return result
+    #return ''.join(c for c in originalPath if c.isalpha())
+
+for filename in listdir(folder_path):
+    src = folder_path + filename
+    dst = folder_path + getModifiedPath(filename)
+    print(f" Renamed: {src} to ===> {dst}")
+
+    os.rename(src,dst)
